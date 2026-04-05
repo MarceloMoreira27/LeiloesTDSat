84af4b20edae2cac3047f3807fce1f4fd38189e5
Marcelo Moreira <celomagik@hotmail.com>
2026-04-05 01:47:31 -0300

    Commit #5: Ajuste para aproveitamento do método listar para produtos vendidos

--- src/vendasVIEW.java
+++ src/vendasVIEW.java
@@ -18,7 +18,7 @@ public class vendasVIEW extends javax.swing.JFrame {
      */
     public vendasVIEW() {
         initComponents();
-        listarProdutos();
+        listarProdutosVendidos();
     }
 
     /**
@@ -142,14 +142,14 @@ public class vendasVIEW extends javax.swing.JFrame {
     private javax.swing.JTable listaProdutos;
     // End of variables declaration//GEN-END:variables
 
-    private void listarProdutos() {
+    private void listarProdutosVendidos(){
         try {
             ProdutosDAO produtosdao = new ProdutosDAO();
 
             DefaultTableModel model = (DefaultTableModel) listaProdutos.getModel();
             model.setNumRows(0);
 
-            ArrayList<ProdutosDTO> listagem = produtosdao.listarProdutos();
+            ArrayList<ProdutosDTO> listagem = produtosdao.listarProdutosVendidos();
 
             for (int i = 0; i < listagem.size(); i++) {
                 model.addRow(new Object[]{
